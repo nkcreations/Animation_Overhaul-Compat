cb84ce8046f37c999f80240a31411e7f0e8476cc
nkcreations <nikoramz@gmail.com>
2025-08-03 19:35:51 -0400

    last changes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,15 +10,28 @@ pluginManagement {
     val egtVersion = "0.2.2"
     plugins {
         id("gg.essential.multi-version.root") version egtVersion
+        id("gg.essential.multi-version") version egtVersion
+        id("org.jetbrains.kotlin.jvm") version "1.9.24"
+        id("com.github.johnrengelman.shadow") version "8.1.1"
     }
-    dependencyResolutionManagement {
-        versionCatalogs {
-            create("libs")
-            create("egt") {
-                plugin("multiversion", "gg.essential.multi-version").version(egtVersion)
-                plugin("multiversionRoot", "gg.essential.multi-version.root").version(egtVersion)
-                plugin("defaults", "gg.essential.defaults").version(egtVersion)
-            }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        mavenCentral()
+        maven("https://maven.fabricmc.net")
+        maven("https://maven.architectury.dev/")
+        maven("https://maven.minecraftforge.net")
+        maven("https://repo.essential.gg/repository/maven-public")
+        maven("https://api.modrinth.com/maven")
+    }
+    versionCatalogs {
+        create("libs")
+        create("egt") {
+            plugin("multiversion", "gg.essential.multi-version").version(egtVersion)
+            plugin("multiversionRoot", "gg.essential.multi-version.root").version(egtVersion)
+            plugin("defaults", "gg.essential.defaults").version(egtVersion)
         }
     }
 }
@@ -30,9 +43,7 @@ rootProject.buildFileName = "root.gradle.kts"
 
 listOf(
     "1.20.1-fabric",
-    "1.20.1-forge",
-    "1.19.2-forge",
-    "1.19.2-fabric"
+    "1.20.1-forge"
 ).forEach { version ->
     include(":$version")
     project(":$version").apply {
